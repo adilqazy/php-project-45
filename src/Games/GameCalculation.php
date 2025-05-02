12b9ce1dc7f17e303f7967b1256c699dcc3dc961
keep silent <learn.fllutter@gmail.com>
2025-05-02 21:56:12 +0500

    vix ver 5

--- src/Games/GameCalculation.php
+++ src/Games/GameCalculation.php
@@ -4,21 +4,35 @@ namespace PhpProject\src\Games\GameCalculation;
 
 use function PhpProject\Engine\runGame;
 
+function calculation(int $num1, int $num2, string $operator): int
+{
+    $result = 0;
+    switch ($operator) {
+        case '+':
+            $result = $num1 + $num2;
+            break;
+        case '-':
+            $result = $num1 - $num2;
+            break;
+        case '*':
+            $result = $num1 * $num2;
+            break;
+    }
+    return $result;
+}
+
 function play(): void
 {
     $task = 'What is the result of the expression?';
     $operators = ['+', '-', '*'];
     $rounds = [];
-    $numberOfTries = 3;
 
-    for ($i = 0; $i < $numberOfTries; $i++) {
+    for ($i = 0; $i < 3; $i++) {
         $num1 = rand(1, 20);
         $num2 = rand(1, 20);
         $operator = $operators[array_rand($operators)];
-
         $question = "$num1 $operator $num2";
-        eval("\$correctAnswer = (string)($num1 $operator $num2);");
-
+        $correctAnswer = (string)calculation($num1, $num2, $operator);
         $rounds[] = [$question, $correctAnswer];
     }
 
